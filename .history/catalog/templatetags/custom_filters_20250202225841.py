b24e962b9ae7f2229d24b90e4d3e0aa3e0c15cbf
Mrslimek <viktorr9456@gmail.com>
2025-02-02 23:31:13 +0300

    02.02

---
+++ .history/catalog/templatetags/custom_filters_20250202225841.py
@@ -0,0 +1,35 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def mul(value, arg):
+    return value * arg
+
+@register.filter
+def div(value, arg):
+    return value / arg
+
+@register.filter
+def sub(value, arg):
+    return value - arg
+
+@register.filter
+def add(value, arg):
+    return value + arg
+
+@register.filter(name='stringify')
+def stringify(value):
+    return str(value)
+
+@register.filter(name='lower')
+def lower(value):
+    return value.lower()
+
+@register.filter(name='censor')
+def censor_email(email):
+    local_part, domain = email.split('@')
+    if len(local_part)
+    return value
---
+++ .history/catalog/templatetags/custom_filters_20250202230059.py
@@ -0,0 +1,38 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def mul(value, arg):
+    return value * arg
+
+@register.filter
+def div(value, arg):
+    return value / arg
+
+@register.filter
+def sub(value, arg):
+    return value - arg
+
+@register.filter
+def add(value, arg):
+    return value + arg
+
+@register.filter(name='stringify')
+def stringify(value):
+    return str(value)
+
+@register.filter(name='lower')
+def lower(value):
+    return value.lower()
+
+@register.filter(name='censor')
+def censor_email(email):
+    local_part, domain = email.split('@')
+    if len(local_part) > 2:
+        censored_local = local_part[0] + '*' * (len(local_part) - 2) + local_part[-1]
+    else:
+        censored_local
+    return value
